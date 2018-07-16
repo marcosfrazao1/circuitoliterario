07641c15c43747b277ee9d6aa466b3eda435f774
Pacarsa <pacarsait@gmail.com>
2018-07-16 08:18:57 -0300

    First Try at NavPush to Page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Timeline } from '../timeline/timeline';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  TimelineButton: any;
 
   constructor(public navCtrl: NavController) {
-
+    this.TimelineButton = Timeline
   }
 
 }
---
+++ src/pages/timeline/timeline.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'page-timeline',
+    templateUrl: 'timeline.html'
+})
+export class Timeline {
+    TimelineButton: any;
+
+    constructor(public navCtrl: NavController) {
+        this.TimelineButton = Timeline;
+
+
+
+
+
+    }
+
+
+}
