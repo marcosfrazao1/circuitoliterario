a1bceb7df4008634dfcee8a069767aa3dfc8517e
Marcos <frazaomarcos1@gmail.com>
2018-08-01 13:36:21 -0300

    time

--- src/pages/timeline/timeline.ts
+++ src/pages/timeline/timeline.ts
@@ -184,10 +184,7 @@ export class Timeline {
         },
         {
             title: 'Efrém da Síria',
-            content: `Parsley amaranth tigernut silver beet maize fennel spinach. Ricebean black-eyed pea maize
-                scallion green bean spinach cabbage jícama bell pepper carrot onion corn plantain garbanzo.
-                Sierra leone bologi komatsuna celery peanut swiss chard silver beet squash dandelion maize
-                chicory burdock tatsoi dulse radish wakame beetroot.`,
+            ccontent: `../assets/timelineimgs/efrem.jpg`,
             icon: 'calendar',
             time: { title: 'séc IV' }
         },
