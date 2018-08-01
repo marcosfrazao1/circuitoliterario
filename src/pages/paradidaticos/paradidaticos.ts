617427b038c362288c79dbbef1956d67f9ca63a4
Pacarsa <pacarsait@gmail.com>
2018-08-01 11:08:08 -0300

    Preparação da pagina Paradidaticos para implementação do conteudo

--- src/pages/paradidaticos/paradidaticos.ts
+++ src/pages/paradidaticos/paradidaticos.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http'
+import 'rxjs/add/operator/map'
 import { HomePage } from '../home/home';
 import { Timeline } from '../timeline/timeline'
 import { Projetos } from '../projetos/projetos';
@@ -12,13 +14,23 @@ import { Fale } from '../fale/fale';
     templateUrl: 'paradidaticos.html'
 })
 export class Paradidaticos {
+    information: any[];
 
-
-    constructor(public navCtrl: NavController) {
-
-
+    constructor(public navCtrl: NavController, private http: Http) {
+        let localData = http.get('assets/paradidaticos.json').map(res => res.json().items);
+        localData.subscribe(data => {
+            this.information = data;
+        })
     }
   
+    toggleSection(i) {
+        this.information[i].open = !this.information[i].open;
+    }
+
+    toggleItem(i, j) {
+        this.information[i].children[j].open = !this.information[i].children[j].open;
+    }
+
     TimelineButton() {
         this.navCtrl.setRoot(Timeline)
     }
