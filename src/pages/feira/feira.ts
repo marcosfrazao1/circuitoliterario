a15b828cbf41cc9626180d99eab2b005612c1d9e
Pacarsa <pacarsait@gmail.com>
2018-08-03 10:21:40 -0300

    Background Feira

--- src/pages/feira/feira.ts
+++ src/pages/feira/feira.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { HomePage } from '../home/home';
-import { Timeline } from '../timeline/timeline'
-import { Paradidaticos } from '../paradidaticos/paradidaticos';
-import { Projetos } from '../projetos/projetos';
-import { Atividades } from '../atividades/atividades';
-import { Mapa } from '../mapa/mapa';
-import { Fale } from '../fale/fale';
 
 @Component({
     selector: 'page-feira',
@@ -20,27 +13,5 @@ export class Feira {
 
 
     }
-  
-    TimelineButton() {
-        this.navCtrl.setRoot(Timeline)
-    }
-    HomeButton() {
-        this.navCtrl.setRoot(HomePage)
-    }
-    ParadidaticosButton() {
-        this.navCtrl.setRoot(Paradidaticos)
-    }
-    ProjetosButton(){
-        this.navCtrl.setRoot(Projetos)
-    }
-    AtividadesButton(){
-        this.navCtrl.setRoot(Atividades)
-    }
-    MapaButton(){
-        this.navCtrl.setRoot(Mapa)
-    }
-    FaleButton(){
-        this.navCtrl.setRoot(Fale)
-    }
-  
+
   }
