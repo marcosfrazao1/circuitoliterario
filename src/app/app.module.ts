76cf607777bd53516bfa7bcd1fe3a0fd5b194709
Marcos <frazaomarcos1@gmail.com>
2018-08-10 15:47:48 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { HttpModule } from '@angular/http'
 
+
+// Import your library
+import { Ng2Carousel3dModule }  from 'ng2-carousel-3d';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { Timeline } from '../pages/timeline/timeline';
@@ -37,6 +41,7 @@ import { TimelineComponent, TimelineItemComponent, TimelineTimeComponent } from 
     BrowserModule,
     HttpModule,
     IonicModule.forRoot(MyApp)
+   
   ],
   bootstrap: [IonicApp],
   entryComponents: [
