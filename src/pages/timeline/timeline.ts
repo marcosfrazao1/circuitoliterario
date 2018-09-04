b213acf7ff3cc603bbe5c55658ac0ab3b8889c57
Pacarsa <pacarsait@gmail.com>
2018-09-04 08:36:11 -0300

    Mudança da Localização das imagens para serem carregadas corretamente no build

--- src/pages/timeline/timeline.ts
+++ src/pages/timeline/timeline.ts
@@ -10,277 +10,277 @@ export class Timeline {
     items = [
         {
             title: 'Homero',
-            content: `../assets/timelineimgs/homero.jpg`,
+            content: `assets/timelineimgs/homero.jpg`,
             icon: 'calendar',
             time: { title: 'VIII aC' }
         },
         {
             title: 'Hesíodo',
-            content: `../assets/timelineimgs/hesiodo.jpg`,
+            content: `assets/timelineimgs/hesiodo.jpg`,
             icon: 'calendar',
             time: { title: 'VIII aC' }
         },
         {
             title: 'Esopo',
-            content: `../assets/timelineimgs/esopo.jpg`,
+            content: `assets/timelineimgs/esopo.jpg`,
             icon: 'calendar',
             time: { title: 'VII aC' }
         },
         {
             title: 'Bíblia',
-            content: `../assets/timelineimgs/testamento.jpg`,
+            content: `assets/timelineimgs/testamento.jpg`,
             icon: 'calendar',
             time: { title: 'VI aC' }
         },
         {
             title: 'Píndaro',
-            content: `../assets/timelineimgs/pindaro.jpg`,
+            content: `assets/timelineimgs/pindaro.jpg`,
             icon: 'calendar',
             time: { title: 'VI aC' }
         },
         {
             title: 'Ésquilo',
-            content: `../assets/timelineimgs/esquilo.jpg`,
+            content: `assets/timelineimgs/esquilo.jpg`,
             icon: 'calendar',
             time: { title: '472 aC' }
         },
         {
             title: 'Sófocles',
-            content: `../assets/timelineimgs/sofocles.jpg`,
+            content: `assets/timelineimgs/sofocles.jpg`,
             icon: 'calendar',
             time: { title: '446 aC' }
         },
         {
             title: 'Eurípedes',
-            content: `../assets/timelineimgs/euripedes.jpg`,
+            content: `assets/timelineimgs/euripedes.jpg`,
             icon: 'calendar',
             time: { title: '438 aC' }
         },
         {
             title: 'Aristófanes',
-            content: `../assets/timelineimgs/aristofanes.jpg`,
+            content: `assets/timelineimgs/aristofanes.jpg`,
             icon: 'calendar',
             time: { title: '425 aC' }
         },
         {
             title: 'Calímaco',
-            content: `../assets/timelineimgs/calimaco.jpg`,
+            content: `assets/timelineimgs/calimaco.jpg`,
             icon: 'calendar',
             time: { title: 'III aC' }
         },
         {
             title: 'Teócrito',
-            content: `../assets/timelineimgs/teocrito.jpg`,
+            content: `assets/timelineimgs/teocrito.jpg`,
             icon: 'calendar',
             time: { title: 'III aC' }
         },
         {
             title: 'Plauto',
-            content: `../assets/timelineimgs/plauto.jpg`,
+            content: `assets/timelineimgs/plauto.jpg`,
             icon: 'calendar',
             time: { title: '205 aC' }
         },
         {
             title: 'Terêncio',
-            content: `../assets/timelineimgs/terencio.jpg`,
+            content: `assets/timelineimgs/terencio.jpg`,
             icon: 'calendar',
             time: { title: 'II aC' }
         },
         {
             title: 'Catulo',
-            content: `../assets/timelineimgs/catulo.jpg`,
+            content: `assets/timelineimgs/catulo.jpg`,
             icon: 'calendar',
             time: { title: 'I aC' }
         },
         {
             title: 'Varrão',
-            content: `../assets/timelineimgs/varrao.jpg`,
+            content: `assets/timelineimgs/varrao.jpg`,
             icon: 'calendar',
             time: { title: 'I aC' }
         },
         {
             title: 'Cícero',
-            content: `../assets/timelineimgs/cicero.jpg`,
+            content: `assets/timelineimgs/cicero.jpg`,
             icon: 'calendar',
             time: { title: '81 aC' }
         },
         {
             title: 'Virgílio',
-            content: `../assets/timelineimgs/virgilio.jpg`,
+            content: `assets/timelineimgs/virgilio.jpg`,
             icon: 'calendar',
             time: { title: '42 aC' }
         },
         {
             title: 'Horácio',
-            content: `../assets/timelineimgs/horacio.jpg`,
+            content: `assets/timelineimgs/horacio.jpg`,
             icon: 'calendar',
             time: { title: '35 aC' }
         },
         {
             title: 'Ovídio',
-            content: `../assets/timelineimgs/ovidio.jpg`,
+            content: `assets/timelineimgs/ovidio.jpg`,
             icon: 'calendar',
             time: { title: '19 aC' }
         },
         {
             title: 'Sêneca',
-            content: `../assets/timelineimgs/seneca.jpg`,
+            content: `assets/timelineimgs/seneca.jpg`,
             icon: 'calendar',
             time: { title: 'sec I' }
         },
         {
             title: 'Estácio',
-            content: `../assets/timelineimgs/estacio.jpg`,
+            content: `assets/timelineimgs/estacio.jpg`,
             icon: 'calendar',
             time: { title: 'sec I' }
         },
         {
             title: 'Luciano de Samostata',
-            content: `../assets/timelineimgs/luciano.jpg`,
+            content: `assets/timelineimgs/luciano.jpg`,
             icon: 'calendar',
             time: { title: 'sec II' }
         },
         {
             title: 'Tertuliano',
-            content: `../assets/timelineimgs/tertuliano.jpg`,
+            content: `assets/timelineimgs/tertuliano.jpg`,
             icon: 'calendar',
             time: { title: 'sec II' }
         },
         {
             title: 'São Jerônimo',
-            content: `../assets/timelineimgs/jeronimo.jpg`,
+            content: `assets/timelineimgs/jeronimo.jpg`,
             icon: 'calendar',
             time: { title: 'sec IV' }
         },
         {
             title: 'Santo Ambrósio',
-            content: `../assets/timelineimgs/ambrosio.jpg`,
+            content: `assets/timelineimgs/ambrosio.jpg`,
             icon: 'calendar',
             time: { title: 'sec IV' }
         },
         {
             title: 'Santo Agostinho',
-            content: `../assets/timelineimgs/agostinho.jpg`,
+            content: `assets/timelineimgs/agostinho.jpg`,
             icon: 'calendar',
             time: { title: 'sec IV' }
         },
         {
             title: 'Claudiano',
-            content: `../assets/timelineimgs/claudiano.jpg`,
+            content: `assets/timelineimgs/claudiano.jpg`,
             icon: 'calendar',
             time: { title: 'sec IV' }
         },
         {
             title: 'Prudêncio',
-            content: `../assets/timelineimgs/prudencio.jpg`,
+            content: `assets/timelineimgs/prudencio.jpg`,
             icon: 'calendar',
             time: { title: 'séc. IV' }
         },
         {
             title: 'Efrém da Síria',
-            content: `../assets/timelineimgs/efrem.jpg`,
+            content: `assets/timelineimgs/efrem.jpg`,
             icon: 'calendar',
             time: { title: 'séc IV' }
         },
         {
             title: 'Agathias',
-            content: `../assets/timelineimgs/agathias.jpg`,
+            content: `assets/timelineimgs/agathias.jpg`,
             icon: 'calendar',
             time: { title: 'séc. VI' }
         },
         {
             title: 'João Malala',
-            content: `../assets/timelineimgs/malalas.jpg`,
+            content: `assets/timelineimgs/malalas.jpg`,
             icon: 'calendar',
             time: { title: '560' }
         },
         {
             title: 'André de Creta',
-            content: `../assets/timelineimgs/andre.jpg`,
+            content: `assets/timelineimgs/andre.jpg`,
             icon: 'calendar',
             time: { title: 'sec VII' }
         },
         {
             title: 'João Damasceno',
-            content: `../assets/timelineimgs/damasceno.jpg`,
+            content: `assets/timelineimgs/damasceno.jpg`,
             icon: 'calendar',
             time: { title: 'sec VII' }
         },
         {
             title: 'Alcuíno de York',
-            content: `../assets/timelineimgs/alcuino.jpg`,
+            content: `assets/timelineimgs/alcuino.jpg`,
             icon: 'calendar',
             time: { title: 'sec VIII' }
         },
         {
             title: 'Beda',
-            content: `../assets/timelineimgs/beda.jpg`,
+            content: `assets/timelineimgs/beda.jpg`,
             icon: 'calendar',
             time: { title: 'sec VIII' }
         },
         {
             title: 'Cynewulf',
-            content: `../assets/timelineimgs/cynewulf.jpg`,
+            content: `assets/timelineimgs/cynewulf.jpg`,
             icon: 'calendar',
             time: { title: 'sec IX' }
         },
         {
             title: 'Photios (“Myrobiblion”)',
-            content: `../assets/timelineimgs/photios.jpg`,
+            content: `assets/timelineimgs/photios.jpg`,
             icon: 'calendar',
             time: { title: 'séc. IX' }
         },
         {
             title: 'Beowolf (anônimo)',
-            content: `../assets/timelineimgs/beowolf.jpg`,
+            content: `assets/timelineimgs/beowolf.jpg`,
             icon: 'calendar',
             time: { title: '1000' }
         },
         {
             title: 'Godofredo de Monmouth',
-            content: `../assets/timelineimgs/godofredo.jpg`,
+            content: `assets/timelineimgs/godofredo.jpg`,
             icon: 'calendar',
             time: { title: '1130' }
         },
         {
             title: 'Prodromos',
-            content: `../assets/timelineimgs/teodoro.jpg`,
+            content: `assets/timelineimgs/teodoro.jpg`,
             icon: 'calendar',
             time: { title: 'sec XII' }
         },
         {
             title: 'Michel Psellos',
-            content: `../assets/timelineimgs/michel.jpg`,
+            content: `assets/timelineimgs/michel.jpg`,
             icon: 'calendar',
             time: { title: 'sec XII' }
         },
         {
             title: 'Allain de Lille',
-            content: `../assets/timelineimgs/allain.jpg`,
+            content: `assets/timelineimgs/allain.jpg`,
             icon: 'calendar',
             time: { title: 'sec XII' }
         },
         {
             title: 'Poema del Cid (anônimo)',
-            content: `../assets/timelineimgs/cid.jpg`,
+            content: `assets/timelineimgs/cid.jpg`,
             icon: 'calendar',
             time: { title: '1140' }
         },
         {
             title: 'Canção dos Nibelungos (anônimo)',
-            content: `../assets/timelineimgs/nibelungos.jpg`,
+            content: `assets/timelineimgs/nibelungos.jpg`,
             icon: 'calendar',
             time: { title: '1180' }
         },
         {
             title: 'Snorri Sturluson)',
-            content: `../assets/timelineimgs/snorri.jpg`,
+            content: `assets/timelineimgs/snorri.jpg`,
             icon: 'calendar',
             time: { title: 'sec XIII' }
         },
         {
             title: 'Saxão, o Gramatico',
-            content: `../assets/timelineimgs/saxao.jpg`,
+            content: `assets/timelineimgs/saxao.jpg`,
             icon: 'calendar',
             time: { title: 'sec XIII' }
         },
